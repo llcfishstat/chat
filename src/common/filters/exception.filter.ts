94e3d75f04e35c8fd25daca99b84682d55895108
rodionros <r.rostovshchikov@yandex.ru>
2025-01-10 20:05:38 +0300

    feat: add auction and chat submodule

--- src/common/filters/exception.filter.ts
+++ src/common/filters/exception.filter.ts
@@ -1,50 +1,16 @@
-import {
-    ArgumentsHost,
-    Catch,
-    ExceptionFilter,
-    HttpException,
-    HttpStatus,
-    Logger,
-} from '@nestjs/common';
-import { Response } from 'express';
-import { I18nService } from 'nestjs-i18n';
+import { ApolloError } from 'apollo-server-express';
+import { ArgumentsHost, Catch, BadRequestException } from '@nestjs/common';
 
-@Catch()
-export class HttpExceptionFilter implements ExceptionFilter {
-    private readonly logger = new Logger(HttpExceptionFilter.name);
+import { GqlExceptionFilter } from '@nestjs/graphql';
+@Catch(BadRequestException)
+export class GraphQLErrorFilter implements GqlExceptionFilter {
+    catch(exception: BadRequestException) {
+        const response = exception.getResponse();
 
-    constructor(private readonly i18n: I18nService) {}
-
-    async catch(exception: unknown, host: ArgumentsHost) {
-        const context = host.switchToHttp();
-        const response = context.getResponse<Response>();
-
-        const statusCode =
-            exception instanceof HttpException
-                ? exception.getStatus()
-                : HttpStatus.INTERNAL_SERVER_ERROR;
-
-        const translationKey =
-            exception instanceof HttpException && exception.message
-                ? exception.message
-                : `http.${HttpStatus.INTERNAL_SERVER_ERROR}`;
-
-        const message = await this.i18n.t(translationKey);
-
-        const errorResponse = {
-            statusCode,
-            timestamp: new Date().toISOString(),
-            message,
-        };
-
-        if (statusCode === HttpStatus.INTERNAL_SERVER_ERROR) {
-            const errorDetails = {
-                ...errorResponse,
-                stack: exception instanceof Error ? exception.stack : undefined,
-            };
-            this.logger.error(JSON.stringify(errorDetails));
+        if (typeof response === 'object') {
+            throw new ApolloError('Validation error', 'VALIDATION_ERROR', response);
+        } else {
+            throw new ApolloError('Bad Request');
         }
-
-        response.status(statusCode).json(errorResponse);
     }
 }
