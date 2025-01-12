96a6a39a4e079dd9fd48ef1cdc25d396ca357d81
rodionros <r.rostovshchikov@yandex.ru>
2025-01-12 14:40:40 +0300

    feat: add chat redis user

--- src/common/config/redis.config.ts
+++ src/common/config/redis.config.ts
@@ -7,6 +7,6 @@ export default registerAs(
     host: process.env.REDIS_HOST ?? 'localhost',
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
     password: process.env.REDIS_PASSWORD,
-    username: process.env.REDIS_USERNAME,
+    user: process.env.REDIS_USER,
   }),
 );
