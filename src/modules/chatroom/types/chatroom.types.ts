b5edcb25ed1b143bdf80cf06549e85bece309554
rodionros <r.rostovshchikov@yandex.ru>
2025-01-15 18:28:24 +0300

    feat: fix chat types

--- src/modules/chatroom/types/chatroom.types.ts
+++ src/modules/chatroom/types/chatroom.types.ts
@@ -2,62 +2,63 @@ import { Field, ObjectType, ID } from '@nestjs/graphql';
 
 @ObjectType()
 export class Chatroom {
-  @Field({ nullable: true })
-  id?: string;
 
-  @Field({ nullable: true })
-  name?: string;
+  @Field(() => String)
+  id: string;
 
-  @Field({ nullable: true })
-  createdAt?: Date;
+  @Field(() => String)
+  name: string;
 
-  @Field({ nullable: true })
-  updatedAt?: Date;
+  @Field(() => Date)
+  createdAt: Date;
 
-  @Field(() => [String], { nullable: true })
-  userIds?: string[];
+  @Field(() => Date)
+  updatedAt: Date;
 
-  @Field(() => [Message], { nullable: true })
-  messages?: Message[];
+  @Field(() => [String])
+  userIds: string[];
+
+  @Field(() => [Message])
+  messages: Message[];
 }
 
 @ObjectType()
 export class Message {
-  @Field(() => ID, { nullable: true })
-  id?: string;
+  @Field(() => String)
+  id: string;
 
-  @Field({ nullable: true })
-  imageUrl?: string;
+  @Field(() => String)
+  imageUrl: string;
 
-  @Field({ nullable: true })
-  content?: string;
+  @Field(() => String)
+  content: string;
 
-  @Field({ nullable: true })
+  @Field(() => Date)
   createdAt?: Date;
 
-  @Field({ nullable: true })
+  @Field(() => Date)
   updatedAt?: Date;
 
-  @Field(() => Chatroom, { nullable: true })
-  chatroom?: Chatroom;
+  @Field(() => Chatroom)
+  chatroom: Chatroom;
 
-  @Field({ nullable: true })
-  userId?: string;
+  @Field(() => String)
+  userId: string;
 }
 
 @ObjectType()
 export class User {
-  @Field({ nullable: true })
-  id?: string;
+  @Field(() => String)
+  id: string;
 }
 
 @ObjectType()
 export class UserTyping {
-  @Field({ nullable: true })
-  userId?: string;
+  @Field(() => String)
+  userId: string;
 
-  @Field({ nullable: true })
-  chatroomId?: number;
+  @Field(() => Number)
+  chatroomId: number;
 }
 
 @ObjectType()
