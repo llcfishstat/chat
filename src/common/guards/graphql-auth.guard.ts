dec42cd2d5912156ea1a4ec5f94ae15c826eed6d
Sergey <dev@skuspekov.ru>
2025-01-24 23:28:36 +0300

    feat: add lgos

--- src/common/guards/graphql-auth.guard.ts
+++ src/common/guards/graphql-auth.guard.ts
@@ -5,53 +5,57 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class GraphqlAuthGuard implements CanActivate {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly configService: ConfigService,
-  ) {}
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+    ) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const gqlContext = GqlExecutionContext.create(context);
-    const { req, connection } = gqlContext.getContext();
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const gqlContext = GqlExecutionContext.create(context);
+        const { req, connection } = gqlContext.getContext();
 
-    const token = this.extractTokenFromHeader(req.headers);
+        console.log({ req });
 
-    if (!token) {
-      throw new UnauthorizedException('Token not provided');
+        const token = this.extractTokenFromHeader(req.headers);
+
+        console.log({ token });
+
+        if (!token) {
+            throw new UnauthorizedException('Token not provided');
+        }
+        console.log(this.configService.get<string>('auth.accessToken.secret'));
+
+        try {
+            const secret = this.configService.get<string>('auth.accessToken.secret');
+            const payload = await this.jwtService.verifyAsync(token, { secret });
+            if (req) {
+                req.user = payload;
+            } else if (connection) {
+                connection.context.user = payload;
+            }
+        } catch (err) {
+            console.error('JWT verification failed:', err);
+            throw new UnauthorizedException('Token verification failed');
+        }
+
+        return true;
     }
-    console.log(this.configService.get<string>('auth.accessToken.secret'));
 
-    try {
-      const secret = this.configService.get<string>('auth.accessToken.secret');
-      const payload = await this.jwtService.verifyAsync(token, { secret });
-      if (req) {
-        req.user = payload;
-      } else if (connection) {
-        connection.context.user = payload;
-      }
-    } catch (err) {
-      console.error('JWT verification failed:', err);
-      throw new UnauthorizedException('Token verification failed');
+    private extractTokenFromHeader(headers: { [key: string]: string }): string | undefined {
+        if (headers['authorization'] && headers['authorization'].startsWith('Bearer ')) {
+            return headers['authorization'].replace('Bearer ', '').trim();
+        }
+
+        if (headers['cookie']) {
+            const cookies = headers['cookie'].split(';').reduce((acc, cookie) => {
+                const [key, value] = cookie.split('=');
+                acc[key.trim()] = value;
+                return acc;
+            }, {});
+
+            if (cookies['accessToken']) {
+                return cookies['accessToken'];
+            }
+        }
     }
-
-    return true;
-  }
-
-  private extractTokenFromHeader(headers: { [key: string]: string }): string | undefined {
-    if (headers['authorization'] && headers['authorization'].startsWith('Bearer ')) {
-      return headers['authorization'].replace('Bearer ', '').trim();
-    }
-
-    if (headers['cookie']) {
-      const cookies = headers['cookie'].split(';').reduce((acc, cookie) => {
-        const [key, value] = cookie.split('=');
-        acc[key.trim()] = value;
-        return acc;
-      }, {});
-
-      if (cookies['accessToken']) {
-        return cookies['accessToken'];
-      }
-    }
-  }
 }
