8c9319c9840bd8b77d4a8a1839a0d5956011668f
Sergey <dev@skuspekov.ru>
2025-01-25 00:26:44 +0300

    feat: add auth service

--- src/modules/chatroom/chatroom.module.ts
+++ src/modules/chatroom/chatroom.module.ts
@@ -4,16 +4,30 @@ import { ChatroomResolver } from 'src/modules/chatroom/resolvers/chatroom.resolv
 import { PrismaService } from 'src/common/services/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { CommonModule } from 'src/common/common.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    CommonModule,
-  ],
-  providers: [
-    ChatroomService,
-    ChatroomResolver,
-    PrismaService,
-    JwtService,
-  ],
+    imports: [
+        CommonModule,
+        ClientsModule.registerAsync([
+            {
+                name: 'AUTH_SERVICE',
+                imports: [ConfigModule],
+                useFactory: async (configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [`${configService.get('rmq.uri')}`],
+                        queue: `${configService.get('rmq.auth')}`,
+                        queueOptions: {
+                            durable: false,
+                        },
+                    },
+                }),
+                inject: [ConfigService],
+            },
+        ]),
+    ],
+    providers: [ChatroomService, ChatroomResolver, PrismaService, JwtService],
 })
 export class ChatroomModule {}
